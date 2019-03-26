20f51358303189504a2ac5a47dbf82a075e34c60
kaperskyguru <solomoneseme@gmail.com>
2019-03-26 22:34:11 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+
+http.listen(3000, () => {
+    console.log('Listening on port *: 3000');
+});
+
+io.on('connection', (socket) => {
+
+    socket.emit('connections', Object.keys(io.sockets.connected).length);
+
+    socket.on('disconnect', () => {
+        console.log("A user disconnected");
+    });
+
+    socket.on('chat-message', (data) => {
+        socket.broadcast.emit('chat-message', (data));
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', (data));
+    });
+
+    socket.on('stopTyping', () => {
+        socket.broadcast.emit('stopTyping');
+    });
+
+    socket.on('joined', (data) => {
+        socket.broadcast.emit('joined', (data));
+    });
+
+    socket.on('leave', (data) => {
+        socket.broadcast.emit('leave', (data));
+    });
+
+});
